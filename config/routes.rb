0796439e6e6531cacb83b5c8580871f6f384c5ff
kangaloos <166692206+kangaloos@users.noreply.github.com>
2024-06-01 08:07:07 +0000

    課題：Bookersを作成しよう　rspec済

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'homes#top'
+  root to:'homes#top'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
